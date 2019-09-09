234ebd91a05c1e054f8e3b17aea1cfeebf52ef71
willtwr <wrtan.edu@gmail.com>
2019-09-09 13:27:44 +0800

    Add files via upload

---
+++ scripts/build_got10k_imdb_reg_bak.py
@@ -0,0 +1,146 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2017 bily     Huazhong University of Science and Technology
+#
+# Distributed under terms of the MIT license.
+
+"""Save the paths of crops from the ImageNet VID 2015 dataset in pickle format"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import glob
+import os
+import os.path as osp
+import pickle
+import sys
+import numpy as np
+import tensorflow as tf
+
+CURRENT_DIR = osp.dirname(__file__)
+sys.path.append(osp.join(CURRENT_DIR, '..'))
+
+from utils.misc_utils import sort_nicely
+
+
+class Config:
+  ### Dataset
+  # directory where curated dataset is stored
+  dataset_dir = 'data/got10k-Curation'
+  save_dir = 'data/'
+
+
+class DataIter:
+  """Container for dataset of one iteration"""
+  pass
+
+
+class Dataset:
+  def __init__(self, config):
+    self.config = config
+
+  def dataset_iterator(self, video_dirs):
+    anno_dir = 'data/got10k'
+    video_num = len(video_dirs)
+    iter_size = 150
+    iter_num = int(np.ceil(video_num / float(iter_size)))
+    for iter_ in range(iter_num):
+      iter_start = iter_ * iter_size
+      iter_videos = video_dirs[iter_start: iter_start + iter_size]
+
+      data_iter = DataIter()
+      num_videos = len(iter_videos)
+      instance_videos = []
+      instance_ann = []
+      for index in range(num_videos):
+        print('Processing {}/{}...'.format(iter_start + index, video_num))
+        video_dir = iter_videos[index]
+        
+        subdirs = video_dir.split('/')
+        ann_vid_dir = osp.join(anno_dir, subdirs[-2], subdirs[-1])
+        with open(osp.join(ann_vid_dir, 'groundtruth.txt')) as f:
+          ann_content = f.readlines()        
+
+        instance_image_paths = glob.glob(video_dir + '/*.crop.x.jpg')
+
+        # sort image paths by frame number
+        instance_image_paths = sort_nicely(instance_image_paths)
+        
+        assert len(instance_image_paths) == len(ann_content)
+        
+        try:
+          # get annotations
+          bboxs = []
+          for ann in ann_content:
+            ann = ann.strip()
+            bbox = ann.split(',')
+            bbox = [float(bb) for bb in bbox]  # [xmin, ymin, w, h]
+            
+            width = bbox[2]
+            height = bbox[3]
+            
+            ratio = width / height
+            
+            if ratio > 1.:
+              bboxs.append([128., 128., 85., 85. / ratio])
+            else:
+              bboxs.append([128., 128., 85. * ratio, 85.])
+              
+          # get image absolute path
+          #instance_image_paths = [os.path.abspath(p) for p in instance_image_paths]
+          instance_videos.append(instance_image_paths)
+          instance_ann.append(bboxs)
+        except:
+          print('Skipped: ' + video_dir)
+      data_iter.num_videos = len(instance_videos)
+      data_iter.instance_videos = instance_videos
+      data_iter.instance_ann = instance_ann
+      yield data_iter
+
+  def get_all_video_dirs(self, dirtype):
+    ann_dir = os.path.join(self.config.dataset_dir, dirtype)
+    train_dirs = os.listdir(ann_dir)
+    all_video_dirs = [os.path.join(ann_dir, name) for name in train_dirs]
+    return all_video_dirs
+
+
+def main():
+  # Get the data.
+  config = Config()
+  dataset = Dataset(config)
+  
+  ### validation
+  validation_dirs = dataset.get_all_video_dirs('val')  
+  validation_imdb = dict()
+  validation_imdb['videos'] = []
+  validation_imdb['ann'] = []
+  for i, data_iter in enumerate(dataset.dataset_iterator(validation_dirs)):
+    validation_imdb['videos'] += data_iter.instance_videos
+    validation_imdb['ann'] += data_iter.instance_ann
+  validation_imdb['n_videos'] = len(validation_imdb['videos'])
+  validation_imdb['image_shape'] = (255, 255, 3)
+
+  ### train
+  train_dirs = dataset.get_all_video_dirs('train')  
+  train_imdb = dict()
+  train_imdb['videos'] = []
+  train_imdb['ann'] = []
+  for i, data_iter in enumerate(dataset.dataset_iterator(train_dirs)):
+    train_imdb['videos'] += data_iter.instance_videos
+    train_imdb['ann'] += data_iter.instance_ann
+  train_imdb['n_videos'] = len(train_imdb['videos'])
+  train_imdb['image_shape'] = (255, 255, 3)
+
+  if not tf.gfile.IsDirectory(config.save_dir):
+    tf.logging.info('Creating training directory: %s', config.save_dir)
+    tf.gfile.MakeDirs(config.save_dir)
+
+  with open(os.path.join(config.save_dir, 'validation_imdb.pickle'), 'wb') as f:
+    pickle.dump(validation_imdb, f)
+  with open(os.path.join(config.save_dir, 'train_imdb.pickle'), 'wb') as f:
+    pickle.dump(train_imdb, f)
+
+
+if __name__ == '__main__':
+  main()
---
+++ scripts/preprocess_got10k_data.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2017 bily     Huazhong University of Science and Technology
+#
+# Distributed under terms of the MIT license.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import os.path as osp
+import sys
+from glob import glob
+from multiprocessing.pool import ThreadPool
+
+import cv2
+from cv2 import imread, imwrite
+from PIL import Image
+
+CURRENT_DIR = osp.dirname(__file__)
+ROOT_DIR = osp.join(CURRENT_DIR, '..')
+sys.path.append(ROOT_DIR)
+
+from utils.infer_utils import get_crops, Rectangle, convert_bbox_format
+from utils.misc_utils import mkdir_p
+
+
+def process_split(root_dir, save_dir, split):
+  data_dir = osp.join(root_dir, split)
+  video_names = os.listdir(data_dir)
+  video_names = [vn for vn in video_names if '.txt' not in vn]
+
+  for idx, video in enumerate(video_names):
+    print('{split} ({idx}/{total}): Processing {video}...'.format(split=split,
+                                                                  idx=idx, total=len(video_names),
+                                                                  video=video))
+    video_path = osp.join(data_dir, video)
+    jpg_files = glob(osp.join(video_path, '*.jpg'))
+    
+    with open(osp.join(video_path, 'groundtruth.txt')) as f:
+      ann_content = f.readlines()
+
+    for jpg in jpg_files:
+      # Read image
+      img_file = jpg.split('/')[-1]
+      img = None
+
+      # Get all object bounding boxes
+      jpgidx = img_file.split('.')[0]
+      jpgidx = int(jpgidx) - 1
+      ann = ann_content[jpgidx]
+      ann = ann.strip()
+      bbox = ann.split(',')
+      bbox = [int(float(bb)) for bb in bbox]  # [xmin, ymin, w, h]
+
+      track_save_dir = osp.join(save_dir, split, video)
+      mkdir_p(track_save_dir)
+      savename = osp.join(track_save_dir, '{}.crop.x.jpg'.format(img_file))
+      
+      if osp.isfile(savename): 
+        try:
+          im = Image.open(savename)
+          continue  # skip existing images
+        except IOError:
+          os.remove(savename)
+          
+      if img is None:
+        img = imread(jpg)
+        
+      # Get crop
+      target_box = convert_bbox_format(Rectangle(*bbox), 'center-based')
+      crop, _ = get_crops(img, target_box,
+                          size_z=127, size_x=255,
+                          context_amount=0.5)
+      imwrite(savename, crop, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+
+
+if __name__ == '__main__':
+  vid_dir = 'data/got10k'
+
+  # Or, you could save the actual curated data to a disk with sufficient space
+  # then create a soft link in `data/ILSVRC2015-VID-Curation`
+  save_dir = 'data/got10k-Curation'
+
+  pool = ThreadPool(processes=2)
+
+  one_work = lambda a: process_split(vid_dir, save_dir, a)
+
+  results = []
+  results.append(pool.apply_async(one_work, ['val']))
+  results.append(pool.apply_async(one_work, ['train']))
+  ans = [res.get() for res in results]
