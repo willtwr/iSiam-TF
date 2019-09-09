b9d085b97703a80d8a4637fdcef85cb15dc47ea8
willtwr <wrtan.edu@gmail.com>
2019-09-09 13:47:07 +0800

    Update vid_reg.py

--- datasets/vid_reg.py
+++ datasets/vid_reg.py
@@ -5,8 +5,6 @@
 #
 # Distributed under terms of the MIT license.
 
-"""VID Dataset"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
