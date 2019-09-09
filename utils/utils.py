ce7e5fc458e88398a9fa27ea13940a9ab591e895
willtwr <wrtan.edu@gmail.com>
2019-09-09 14:04:42 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2019 WR Tan       National Tsing Hua University
+# Distributed under terms of the MIT license.
+
 import numpy as np
 import unicodedata
 import os
