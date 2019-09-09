f9be11212aaddda9a1dd0b0663dc41c8805130bd
willtwr <wrtan.edu@gmail.com>
2019-09-09 13:48:42 +0800

    Update and rename SiamFC-2base.py to iSiam.py

--- experiments/iSiam.py
+++ experiments/iSiam.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2017 bily     Huazhong University of Science and Technology
-# Test Data Augmentation
+# Copyright © 2019 WR Tan     National Tsing Hua University
+# 
 # Distributed under terms of the MIT license.
 
-"""Train the color model in the SiamFC paper from scratch with Data Augmentation"""
+"""Train the color model in the iSiam paper from scratch with Data Augmentation"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -20,7 +21,7 @@ from trains.siamese_2base.configuration import LOG_DIR
 from trains.siamese_2base.train_siamese_model import ex
 
 if __name__ == '__main__':
-  RUN_NAME = 'SiamFC-2base'
+  RUN_NAME = 'iSiam'
   ex.run(config_updates={'train_config': {'train_dir': osp.join(LOG_DIR, 'track_model_checkpoints', RUN_NAME), },
                          'track_config': {'log_dir': osp.join(LOG_DIR, 'track_model_inference', RUN_NAME), }
                          },
