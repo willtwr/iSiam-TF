f19f257c15b7834984897c1e1617290192dccc9e
willtwr <wrtan.edu@gmail.com>
2019-09-10 10:35:00 +0800

    Update __init__.py

--- benchmarks/oxuva/__init__.py
+++ benchmarks/oxuva/__init__.py
@@ -1,5 +1,2 @@
-#from run_SiamFC_bg_temp import *
-#from run_SiamFC_multi_bg_sup import *
-#from run_SiamFC_multi_temp import *
-from run_SiamFC_nonlinear_upNiter import *
+from run_iSiam_oxuva import *
 
